8e2476e00de95510ff348f2e96fa48505d25ae18
Zhang <jim_h_zhang@163.com>
2023-06-30 23:34:04 +0800

    first commit

---
+++ src/main/java/org/fffd/l23o6/service/impl/TrainServiceImpl.java
@@ -0,0 +1,140 @@
+package org.fffd.l23o6.service.impl;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.fffd.l23o6.dao.RouteDao;
+import org.fffd.l23o6.dao.TrainDao;
+import org.fffd.l23o6.mapper.TrainMapper;
+import org.fffd.l23o6.pojo.entity.RouteEntity;
+import org.fffd.l23o6.pojo.entity.TrainEntity;
+import org.fffd.l23o6.pojo.enum_.TrainType;
+import org.fffd.l23o6.pojo.vo.route.RouteVO;
+import org.fffd.l23o6.pojo.vo.train.AdminTrainVO;
+import org.fffd.l23o6.pojo.vo.train.TrainVO;
+import org.fffd.l23o6.pojo.vo.train.TicketInfo;
+import org.fffd.l23o6.pojo.vo.train.TrainDetailVO;
+import org.fffd.l23o6.service.TrainService;
+import org.fffd.l23o6.util.strategy.train.GSeriesSeatStrategy;
+import org.fffd.l23o6.util.strategy.train.KSeriesSeatStrategy;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import io.github.lyc8503.spring.starter.incantation.exception.BizException;
+import io.github.lyc8503.spring.starter.incantation.exception.CommonErrorType;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TrainServiceImpl implements TrainService {
+    private final TrainDao trainDao;
+    private final RouteDao routeDao;
+
+    @Override
+    public TrainDetailVO getTrain(Long trainId) {
+        TrainEntity train = trainDao.findById(trainId).get();
+        RouteEntity route = routeDao.findById(train.getRouteId()).get();
+        return TrainDetailVO.builder().id(trainId).date(train.getDate()).name(train.getName())
+                .stationIds(route.getStationIds()).arrivalTimes(train.getArrivalTimes())
+                .departureTimes(train.getDepartureTimes()).extraInfos(train.getExtraInfos()).build();
+    }
+
+    @Override
+    public List<TrainVO> listTrains(Long startStationId, Long endStationId, String date) {
+        // TODO
+        // 6/30
+        // First, get all routes contains [startCity, endCity]
+        // Then, Get all trains on that day with the wanted routes
+        List<TrainEntity> trainEntitiesList = trainDao.findAll();
+        List<TrainVO> trainVOList = new ArrayList<>();
+        for(TrainEntity train: trainEntitiesList){
+//            for each train get the route by routeID
+            Long routeID = train.getRouteId();
+            RouteEntity routeEntity = routeDao.findById(routeID).get();
+//            find startStation and endStation in the route
+            int start = routeEntity.getStationIds().indexOf(startStationId);
+            int end = routeEntity.getStationIds().indexOf(endStationId);
+            if(start!=-1 && end!=-1 && start <= end){
+//                success find station && start before end
+                if(Objects.equals(train.getDate(), date)){
+//                    success find date TODO : change trainVo
+                    TrainVO trainVO = TrainMapper.INSTANCE.toTrainVO(train);
+                    trainVO.setStartStationId(startStationId);
+                    trainVO.setEndStationId(endStationId);
+                    trainVO.setDepartureTime(train.getDepartureTimes().get(start));
+                    trainVO.setArrivalTime(train.getArrivalTimes().get(end));
+                    trainVOList.add(trainVO);
+                }
+            }
+        }
+        return trainVOList;
+    }
+
+    @Override
+    public List<AdminTrainVO> listTrainsAdmin() {
+        return trainDao.findAll(Sort.by(Sort.Direction.ASC, "name")).stream()
+                .map(TrainMapper.INSTANCE::toAdminTrainVO).collect(Collectors.toList());
+    }
+
+    @Override
+    public void addTrain(String name, Long routeId, TrainType type, String date, List<Date> arrivalTimes,
+            List<Date> departureTimes) {
+        TrainEntity entity = TrainEntity.builder().name(name).routeId(routeId).trainType(type)
+                .date(date).arrivalTimes(arrivalTimes).departureTimes(departureTimes).build();
+        RouteEntity route = routeDao.findById(routeId).get();
+        if (route.getStationIds().size() != entity.getArrivalTimes().size()
+                || route.getStationIds().size() != entity.getDepartureTimes().size()) {
+            throw new BizException(CommonErrorType.ILLEGAL_ARGUMENTS, "列表长度错误");
+        }
+        entity.setExtraInfos(new ArrayList<String>(Collections.nCopies(route.getStationIds().size(), "预计正点")));
+        switch (entity.getTrainType()) {
+            case HIGH_SPEED:
+                entity.setSeats(GSeriesSeatStrategy.INSTANCE.initSeatMap(route.getStationIds().size()));
+                break;
+            case NORMAL_SPEED:
+                entity.setSeats(KSeriesSeatStrategy.INSTANCE.initSeatMap(route.getStationIds().size()));
+                break;
+        }
+        trainDao.save(entity);
+    }
+
+    @Override
+    public void changeTrain(Long id, String name, Long routeId, TrainType type, String date, List<Date> arrivalTimes,
+                            List<Date> departureTimes) {
+        // TODO: edit train info, please refer to `addTrain` above
+//        6/30
+//        select the train by id and delete 这个方法如果更改消息出错，会把原来火车信息删除，可能有点问题
+//        trainDao.delete(trainDao.getReferenceById(id));
+//        addTrain(name, routeId, type, date, arrivalTimes, departureTimes);
+
+//        这个方法看上去正确一点
+        TrainEntity entity=trainDao.findById(id).get();
+        RouteEntity route=routeDao.findById(routeId).get();
+        if (route.getStationIds().size() != entity.getArrivalTimes().size()
+                || route.getStationIds().size() != entity.getDepartureTimes().size()) {
+            throw new BizException(CommonErrorType.ILLEGAL_ARGUMENTS, "列表长度错误");
+        }
+        entity.setName(name);
+        entity.setRouteId(routeId);
+        entity.setTrainType(type);
+        entity.setDate(date);
+        entity.setArrivalTimes(arrivalTimes);
+        entity.setDepartureTimes(departureTimes);
+
+        entity.setExtraInfos(new ArrayList<String>(Collections.nCopies(route.getStationIds().size(), "预计正点")));
+        switch (type) {
+            case HIGH_SPEED:
+                entity.setSeats(GSeriesSeatStrategy.INSTANCE.initSeatMap(route.getStationIds().size()));
+                break;
+            case NORMAL_SPEED:
+                entity.setSeats(KSeriesSeatStrategy.INSTANCE.initSeatMap(route.getStationIds().size()));
+                break;
+        }
+        trainDao.save(entity);
+    }
+
+    @Override
+    public void deleteTrain(Long id) {
+        trainDao.deleteById(id);
+    }
+}
