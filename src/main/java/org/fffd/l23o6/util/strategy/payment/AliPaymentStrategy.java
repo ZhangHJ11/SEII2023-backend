97c4889c9227369e3e10730a87a3893d2dd841e9
Zhang <jim_h_zhang@163.com>
2023-07-07 22:26:38 +0800

    merge and bug fixed

--- src/main/java/org/fffd/l23o6/util/strategy/payment/AliPaymentStrategy.java
+++ src/main/java/org/fffd/l23o6/util/strategy/payment/AliPaymentStrategy.java
@@ -10,18 +10,17 @@ import org.mapstruct.Mapping;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Random;
+
 @RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
 public class AliPaymentStrategy extends PaymentStrategy{
-    String OrderNo = "202006120020";
-    String LastOrderNo = "202006120020";
+    String OrderNo = "202006120050";
+    String LastOrderNo = "202006120050";
     String URL = "https://openapi-sandbox.dl.alipaydev.com/gateway.do";
     String APP_ID = "9021000122697449";
     String APP_PRIVATE_KEY = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCeEzqsFvdumM7H30eNi2eS8+srk1qwC1Bi5D6J5uhbGlUOF0qQt5N2IWuKz/xqB3qJuIffy58wHkqMvsAnn+IbfUqJn07U+Ji6sJhYL3VCt0UxJAi9Lc5xHUNe22mbzdeZmxevvMYOxmGL9vZBFO6+bNCk9A4/rWAIIK9+nkLSvgsgZE2YRzdVhXAR2XtBBkSPuaFsDpp1kcZZ0g9kIsyHF/VKYCqHNAFMB8gBAEIdalGjHHzmWpMT2SbLVrcAAAyjVnEfN/K3o2g1SRt3COsmfphrZy9jHmVmKf88ahSsKRoUrNIJDsuarLPD9hTe3JegmAJ5dEOGP9vYs8P1jW+ZAgMBAAECggEAL1/B8Ls9Umb3ac1Py2rhEpiJfVi/EA2D/O88kuWh81Oz8/IlBbQOvJONgF+O3VP6dHXaOsbyoWAipYWxXu66u9RT12ToIkmStDiin6MxeFomoXCUMeYdfabwfugxAWGyYLWrZp6jmN/5chIEgUK9vlIsQZNnBcx3AFiTX9YcXr3d0+3rcL0Tit5hmp7lBmckQ+8mN/QqoA9BeUPCH2EKKQ7j/7k8CZlYABOZFAlanra8ft4AnlpAdeTK1bkAcEo2r8MtTJ93fkqZyMtFv3uwe5l6ZEBVMDxyW/029mMTYP7xweFNbHSKq/bRdACLDzNFoujABt8D11WCp2PIpHvgRQKBgQDrZ2glLPPB4MAWbZn8OO2PFc2mp5MWOTPnide4HhqRgCK/h17kNYPodcyETo5biiquG1eMcTqZE3aAy3zA72nvCd4Bdhni7DtLup9RtdTiUfNa9LqT454EKL5bJnTTdk5xWOyGaqNSGz1D3NhjGBgn7zts8W1CQTXJbqEwQmlIowKBgQCr585nZAiF3doHZbRgWIbjp5MIqQ3UBtnNpycK1zPIdRgZ5nD3PoXin4UqO8Pc2dlRyVmFJPER3WLIRaMqEQVjCB4VfondMys3MZFrqm1fDyUlK/rvj0b/IKxQ69hFL06Pb9CEyfd71lo2X4zBia+Vhy3BASoYm+SgIkabrE3+kwKBgGOeUQ5aZZVIXUGghCeel7mTioHaLH6dGoADb0y1QJ0OUqYmzVtZ4kZ/g1sCO3A1mjYvz+7ZA+k/uUhr1JF5P6Uu0cpghjfnrHgxs7ZKcpqL/6YgJszq6DXuYBAGyC2sxSlmbkG74v2eFWEgllnpGl/b95P03z3IhtCFhqT7lb5tAoGAPlwi1gLFzLoEA7bzThgPJrp9biDVcVri2+WNFjOnB0ri6VJdHHPD4cf4aK+xh4ZHfZHLNa+G+/IRiZEpdXMjPx+MukNjA6kyPh1vIV17u4Yk9W0U5J5XJOnXWKuVktNsbkAjpmgVrQi/RQY4a2TlGxskj+U6Ph8fOq/0aTiCOp0CgYEA3Sj12+24z2T78uetllJGRNjW0wVuolv0uTntp1G6cEsSAVS4Cx5uLjRjv+MZOVLElPQNPb6naS8rOT2FVIlsp7nXAl9oZbWZIpXtZ4d+lr27hBP9Z+0ONs62jruypROyQCTfbUroG3ITqm+yFKdJat5G4KdlKwYrquGARAOV1/4=";
     String ALIPAY_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0Fd1psv/8KTiTqMcdSB2Z68he06yAG6QdhT89nozZuG68T02490YQNzMxllSHSkHz/KrYnATa10ZU+fIEXoDFAENH4izm2C3wKs4AJ/3th0mkrUrNPNIJUtwQLeNuQC7dj352E6Hxr867FsuCAIhlM7xxrzNUiGY3nl6dmO/O+yH1PHkrNDueP8tGk+IjpJdzTXxQlXeVTNh5FpMgagTUkuL5TmMZRJSojC7A/VgUWl/ty6r4SBPzPPjCCW9TG+2x3lQUAw7Vn2eAwXcpNXTq4j11kagROcFCZiAedqOioz7rX0MdIwagZV6R9eSwXXz8BfPdV76QRBmgDL7JkJJqQIDAQAB";
 
-    @PostMapping("/do-post-test")
     public String doPostTest(String price){
 //        返回支付链接
         /** 初始化 **/
@@ -32,14 +31,14 @@ public class AliPaymentStrategy extends PaymentStrategy{
         /** 设置业务参数  **/
         AlipayTradePagePayModel model = new AlipayTradePagePayModel();
         /** 商户订单号,商户自定义，需保证在商户端不重复，如：20200612000001 **/
-        LastOrderNo = OrderNo;
+        Random generateRandom = new Random();
+        OrderNo = String.valueOf(Long.parseLong(OrderNo) + generateRandom.nextLong());
+        System.out.println(OrderNo);
         model.setOutTradeNo(OrderNo);
-        OrderNo = String.valueOf(Long.parseLong(LastOrderNo) + 1);
 
         model.setProductCode("FAST_INSTANT_TRADE_PAY");
         model.setSubject("购买车票");
         /** 订单金额，精确到小数点后两位 **/
-        price = "500";
         model.setTotalAmount(price);
         model.setBody("AliPay");
         alipayRequest.setBizModel(model);
